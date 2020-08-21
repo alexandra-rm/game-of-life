4b56d678acbd9046c48786eddc493cd800002d80
kachmazov <kachmazov@elfin.tech>
2020-08-21 23:30:51 +0400

    Small fixes

--- babel.config.js
+++ babel.config.js
@@ -6,9 +6,8 @@ module.exports = {
   ],
   plugins: [
     "@babel/plugin-proposal-class-properties",
-    "@babel/plugin-transform-runtime",
-    ,
     "babel-plugin-emotion",
+    "@babel/plugin-transform-runtime",
   ],
   env: {
     production: {
---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+export const Card = styled.div`
+  border-radius: 10px;
+  padding: 20px;
+  transition: 0.1s;
+
+  :hover {
+    box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.1);
+  }
+`;
