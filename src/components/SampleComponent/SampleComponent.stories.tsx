e8a827b43e4dc7cc36c34fa1a6c43e2dac74c422
kachmazov <kachmazov@elfin.tech>
2020-08-22 12:56:07 +0400

    Remove SampleComponent

--- src/components/SampleComponent/SampleComponent.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { SampleComponent } from "./SampleComponent";
-
-export default {
-  title: "SampleComponent",
-  component: SampleComponent,
-};
-
-export const sampleComponetn = () => <SampleComponent name="Александр" />;
--- src/components/SampleComponent/SampleComponent.tsx
+++
@@ -1,9 +0,0 @@
-import React, { FC } from "react";
-
-export interface SampleComponentProps {
-  name: string;
-}
-
-export const SampleComponent: FC<SampleComponentProps> = ({ name }) => (
-  <h1>Hello, {name}</h1>
-);
