0d7bb6191b1eb59562e06903639ed6d8fa0b0e2a
kachmazov <kachmazov@elfin.tech>
2020-08-23 22:41:06 +0400

    Init pages' structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React, { FC } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { GamePage, HomePage, NotFoundPage } from "@/pages";
 
 const App: FC = () => {
-  return <div>App component</div>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/game" component={GamePage} />
+        <Route path="/" exact component={HomePage} />
+        <Route path="*" component={NotFoundPage} />
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 export { App };
