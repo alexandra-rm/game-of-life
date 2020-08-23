b10735c401491c8c1f22935b542ad98e3b1d6396
kachmazov <kachmazov@elfin.tech>
2020-08-24 01:07:54 +0400

    Remove Cell from components' export

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
 export { Card } from "./Card";
 export { Section } from "./Section";
 export { PercentChart } from "./PercentChart";
-export { Cell } from "./Cell";
 export { GameField } from "./GameField";
 export { Badge } from "./Badge";
 export { HeightMap } from "./HeightMap";
