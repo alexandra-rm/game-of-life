db86576f38f4035735eb412416fb2f1be61cc91f
kachmazov <kachmazov@elfin.tech>
2020-08-26 20:33:11 +0400

    Add Bets' reducer test

---
+++ src/modules/Bets/reducet.test.ts
@@ -0,0 +1,56 @@
+import { actions, reducer, initialState } from "./reducer";
+
+describe("Bets's reducer", () => {
+  it("setBet action", () => {
+    expect(reducer({ ...initialState }, actions.setBet(100))).toEqual({
+      ...initialState,
+      bet: 100,
+    });
+  });
+
+  it("setBetCell action", () => {
+    const cell = { x: 10, y: 11 };
+    expect(reducer({ ...initialState }, actions.setBetCell(cell))).toEqual({
+      ...initialState,
+      betCell: cell,
+    });
+  });
+
+  it("setBetGeneration action", () => {
+    expect(reducer({ ...initialState }, actions.setBetGeneration(50))).toEqual({
+      ...initialState,
+      betGeneration: 50,
+    });
+  });
+
+  it("setIsOpenBetWindow action", () => {
+    expect(
+      reducer({ ...initialState }, actions.setIsOpenBetWindow(true))
+    ).toEqual({
+      ...initialState,
+      isOpenBetWindow: true,
+    });
+  });
+
+  it("setMaxError action", () => {
+    expect(reducer({ ...initialState }, actions.setMaxError(32))).toEqual({
+      ...initialState,
+      maxError: 32,
+    });
+  });
+
+  it("setAllowBet action", () => {
+    expect(reducer({ ...initialState }, actions.setAllowBet(false))).toEqual({
+      ...initialState,
+      allowBet: false,
+    });
+  });
+
+  it("setMaxError action", () => {
+    expect(reducer({ ...initialState }, actions.confirmBet())).toEqual({
+      ...initialState,
+      allowBet: false,
+      isOpenBetWindow: false,
+    });
+  });
+});
